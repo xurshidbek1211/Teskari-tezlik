51a8185c77e620f320bad4a2a280730d537780c7
xurshidbek1211 <mirjon9999@gmail.com>
2025-07-06 06:52:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 import os
 import random
 
-API_TOKEN = 7265238026:AAE4n-lQd--ViqQgyFhB51XnURFcRdM8Cp8
+API_TOKEN = "7265238026:AAE4n-lQd--ViqQgyFhB51XnURFcRdM8Cp8"
 ADMIN_ID = 1899194677
 
 bot = Bot(token=API_TOKEN)
