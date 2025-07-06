7f79d40f9cf3dd5ae91c80d24c9c9248f802773f
xurshidbek1211 <mirjon9999@gmail.com>
2025-07-06 07:07:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -73,6 +73,7 @@ async def javobni_tekshir(message: types.Message):
             await message.reply("✅ To‘g‘ri! Sizga 1 ball qo‘shildi.")
         else:
             await message.reply("❌ Noto‘g‘ri. Yana urinib ko‘ring.")
-            if __name__ == '__main__':
-    from aiogram import executor
+
+# 🔥 BU YERNI EHTIYOT BO‘LIB QO‘YING
+if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
